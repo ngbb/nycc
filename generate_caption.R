2316e9847d42c9768cae1720f2ce5ef08280fc59
Neil Baron <neil.g.baron@gmail.com>
2017-04-21 13:05:04 -0500

    Keep only original captions from generated sample

--- generate_caption.R
+++ generate_caption.R
@@ -30,12 +30,18 @@ all_text <- str_c(caps$Caption, collapse = " ")
 captionNgram <- ngram(all_text)
 
 # split a babble into separate sentences, remove first and last partial sentences
-split_by_sentence <- function (text) {
+split_by_sentence <- function (text, removereal = TRUE) {
   result <- unlist(strsplit(text, '(?<=[!?.])[[:space:]]*', perl = TRUE))
   result <- result[!str_detect(result, "^(\\&|\\'|\\`)")]
   result <- result[!str_detect(result, "^[a-z]")]
   result <- result[2:(length(result) - 1)]
   result <- result[which(nchar(result) > 2)]
+
+  # remove "real" captions
+  if(removereal) {
+    result <- result[!str_detect(all_text, result)]
+  }
+  
   return(result)
 }
 
@@ -53,7 +59,7 @@ makeCaption <- function(caplist, cutofflength = 7) {
 
 # tweet image + caption
 sendTweet <- function() {
-  gencap1 <- makeCaption(split_by_sentence(babble(captionNgram, genlen = 1000)))
+  gencap1 <- makeCaption(split_by_sentence(babble(captionNgram, genlen = 1500)))
   img <- sample(imgs$Image, 1)
   tempname <- paste0(sample(100000:999999, 1), ".jpg")
   tempfile <- paste0(topdir, tempname)
@@ -67,4 +73,3 @@ sendTweet <- function() {
 
 # run sendTweet function
 sendTweet()
-
